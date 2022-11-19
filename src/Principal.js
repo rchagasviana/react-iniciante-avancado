e22d7640a0bbc9639eb88a406a8f551118424d8f
rafael.c.viana <rchagas.sdi@gmail.com>
2022-11-19 08:14:30 -0300

    Aula 01-Propiedades-props

--- src/Principal.js
+++ src/Principal.js
@@ -1,11 +1,18 @@
 
+const Titulo = (props)=>{
+    return (
+        <div>
+            <h1>{props.conteudo}</h1>
+        </div>
 
+    );
+}
 
 function Principal() {
 
   return (
       <div>
-        Aula 01
+        <Titulo conteudo="Aula 01 - React do básico ao avançado"/>
       </div>
   );
 }
