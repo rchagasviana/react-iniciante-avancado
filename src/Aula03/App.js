448e50a812222bf10764d2d4a0872a4107ddbd7c
rafael.c.viana <rchagas.sdi@gmail.com>
2022-11-19 12:40:25 -0300

    Aula 03-Componentização, Propriedades e Estilos

---
+++ src/Aula03/App.js
@@ -0,0 +1,15 @@
+import Titulo from "./Titulo";
+import Equipe from "./Equipe";
+
+function App(props) {
+    return (
+        <div>
+            <Titulo texto="Aula 03 - Propriedades e Componentes" cor="blue" fonte="Fantasy"/>
+            <Equipe nome="Rafael C. Viana" dataNascimento="10-10-1990" profissao="Engenheiro de Software"/>
+            <Equipe nome="Rafael C. Viana" dataNascimento="11-11-1990" profissao="Engenheiro de Software"/>
+            <Equipe nome="Rafael C. Viana" dataNascimento="12-12-1990" profissao="Engenheiro de Software"/>
+            <Equipe nome="Rafael C. Viana" dataNascimento="06-06-1990" profissao="Engenheiro de Software"/>
+        </div>
+    );
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Aula02/App';
+import App from './Aula03/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
